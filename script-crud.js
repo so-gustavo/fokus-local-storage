b7d8a65e0a13fc453fd3b122df8a4092eae2c9b4
Gustavo So <so.gustavosantos@gmail.com>
2024-01-24 16:57:46 -0300

    submetendo o formulário

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const buttonAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+tarefas = []
 
 buttonAdicionarTarefa.addEventListener('click', () => {
-    formAdicionarTarefa.classList.toggle('hidden')
+    formAdicionarTarefa.classList.toggle('hidden');
 });
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    };
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas)
+})
